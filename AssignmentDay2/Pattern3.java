a8cd39a7e9c09889e8345c378a42c00a48dfbc61
gosaviakshay <109201127+gosaviakshay@users.noreply.github.com>
2023-09-06 20:42:11 +0530

    Add files via upload

---
+++ AssignmentDay2/Pattern3.java
@@ -0,0 +1,57 @@
+package AssignmentDay2;
+
+public class Pattern3 {
+
+	public static void main(String[] args) {
+		
+		for(int i=1;i<=5;i++) {
+			
+			for(int j=5;j>=1;--j) {
+				
+				if(i==j) {
+					System.out.print(""+j);
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+			
+			for(int j=2;j<=5;j++) {
+			
+				if(j==i) {
+					System.out.print(""+j);
+				}
+				else {
+					System.out.print(" ");
+			}
+			}
+			System.out.println();
+		}
+		
+		for(int i=4;i>=1;i--) {
+			for(int j=5;j>=1;j--) {
+				if(i==j)
+				{
+					System.out.print(""+j);
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+			for(int j=2;j<=5;j++) {
+				if(i==j)
+				{
+					System.out.print(""+j);
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+			
+			System.out.println();
+			
+		}
+
+	}
+
+}
